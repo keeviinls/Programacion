06914cb267a95ca7fbded6aaae5cbdda8c79aa51
KevinLoayzaS <kevin28042001@gmail.com>
2022-10-13 14:13:38 +0200

    Subida 10/10

---
+++ src/Session1310/BucleInfinito.java
@@ -0,0 +1,10 @@
+package Session1310;
+
+public class BucleInfinito {
+    public static void main(String[] args) {
+        for (;;)
+        {
+            System.out.println("Bucle infinito");
+        }
+    }
+}
---
+++ src/Session1310/ForIncrementalDecremental.java
@@ -0,0 +1,30 @@
+package Session1310;
+
+import java.util.Scanner;
+
+public class ForIncrementalDecremental {
+    public static void main(String[] args) {
+
+        int numero=0;
+
+
+        //bucle incremental
+
+        Scanner sc = new Scanner(System.in);
+        numero = sc.nextInt();
+
+        System.out.println("Vamos a mostrar por pantalla los numeros de 1 a " + numero);
+
+        for (int i=1;i<=numero;i++)
+        {
+            System.out.println(i);
+        }
+
+        //bucle decremental
+        System.out.println("Vamos a mostrar por pantalla los numeros de " + numero + " a 1");
+        for (int j=numero;j>0;j--)
+        {
+            System.out.println(j);
+        }
+    }
+}
