66a8e6cdc2b1f8a6dbca7c700a131d84a999340e
KevinLoayzaS <kevin28042001@gmail.com>
2022-10-14 13:12:24 +0200

    Subida 14/10

---
+++ src/Session1010/Estructuraif.java
@@ -0,0 +1,24 @@
+package Session1010;
+
+import java.util.Scanner;
+
+public class Estructuraif {
+    public static void main(String[] args) {
+        int a,b,mayor,menor=0;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Introduzca el valor de a: " );
+        a= sc.nextInt();
+        System.out.println("Introduzca el valor de b: ");
+        b=sc.nextInt();
+        mayor=a;
+        menor = b;
+        if (a<b){
+            mayor=b;
+            menor=a;
+        }
+        if (a==b){
+            System.out.println("los numeros son iguales");
+        }
+        System.out.println("El mayor es "+mayor+" y el numero menor es "+ menor);
+    }
+}
