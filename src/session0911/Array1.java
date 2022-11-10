658ef37a87315c35424ade74927035d82471194e
KevinLoayzaS <kevin28042001@gmail.com>
2022-11-10 14:09:24 +0100

    03/11

---
+++ src/session0911/Array1.java
@@ -0,0 +1,32 @@
+package session0911;
+
+import java.util.Scanner;
+
+public class Array1 {
+    public static void main(String[] args) {
+
+
+
+        int [] notas_1ertrimestre = new int [5];
+
+        notas_1ertrimestre [0]=7;
+        notas_1ertrimestre [1]=8;
+        notas_1ertrimestre [2]=6;
+        notas_1ertrimestre [3]=5;
+        notas_1ertrimestre [4]=5;
+
+        int [] notas_2dotrimestre ={7,8,9,7,4};
+
+
+
+        int [] notas_3ertrimestre = new int [5];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i =0;i<notas_3ertrimestre.length;i++)
+        {
+            System.out.println("Introduzcame su nota por favor");
+            notas_3ertrimestre[i]= sc.nextInt();
+
+        }
+    }
+}
