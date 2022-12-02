37c298630f7f1aebc9aee38955eb2fba86c7dcee
KevinLoayzaS <kevin28042001@gmail.com>
2022-12-02 14:07:35 +0100

    2/12

---
+++ src/Session2811/examenejercicio2.java
@@ -0,0 +1,35 @@
+package Session2811;
+
+import java.util.Scanner;
+
+public class examenejercicio2 {
+    public static void main(String[] args) {
+       int opcion;
+
+        do {
+        System.out.println("Funcionalidades ejercicio 2");
+        System.out.println("1. calcular sumatorio");
+        System.out.println("2. invertir array");
+        System.out.println("3. salir");
+
+        System.out.println("Introduzca un elemento");
+        Scanner sc = new Scanner(System.in);
+        opcion= sc.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Que numero quieres el sumatorio")
+                    int numero = sc.nextInt();
+
+                    int sumatorio = metodo1(numero);
+
+                    break;
+                case 2: break;
+                case 3:
+                    System.out.println("Gracias por utilizar el programa.");break;
+
+            }
+
+        }while (opcion==3);
+    }
+}
