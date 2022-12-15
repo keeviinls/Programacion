4862f7dfcd6ec0504c1a3fb1a67dca19244118d4
KevinLoayzaS <kevin28042001@gmail.com>
2022-12-15 14:04:29 +0100

    PROGRAMACION DE CLASE

--- src/ExamenCorregido/Ejercicio3.java
+++ src/ExamenCorregido/Ejercicio3.java
@@ -11,6 +11,66 @@ public class Ejercicio3 {
         int array [] = new int[dimension];
         array=inicializar(array);
         System.out.println(Arrays.toString(array));
+
+        imprimir_multiplos(array);
+
+        numeros_negativos(array);
+
+        int[] array7={4,6,6,4};
+        verificar_capicua(array7);
+    }
+
+    private static void verificar_capicua(int[] array){
+
+        int[] array_invertido = new int[array.length];
+        int j=array.length;
+        for (int i=0;i<array_invertido.length;i++){
+
+            array_invertido[i]=array[i];
+            j--;
+        }
+
+        if (Arrays.equals(array,array)){
+            System.out.println("Es capicua");
+        }
+
+        else {
+            System.out.println("No es capicua");
+        }
+
+    }
+
+
+
+    private static void numeros_negativos(int [] array){
+
+        int contador=0;
+        for (int i=0; i<array.length;i++){
+            if (array[i]<0){
+                contador ++;
+            }
+        }
+
+        if (contador==0){
+            System.out.println("No hay negativos en el array");
+        }
+
+        else{
+            System.out.println("El numero de negativos es " + contador);
+        }
+    }
+
+    public static void imprimir_multiplos(int [] array){
+
+        for (int i=0;i<array.length;i++){
+
+            if ((array[i] % 3 == 0)&&(array[i] % 5==0)){
+                System.out.println("\u001B[31m"+array[i]+"\u001B[0m");
+
+            }
+
+        }
+
     }
 
     private static int[] inicializar(int [] array){
