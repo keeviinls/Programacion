eb02923b37f94e0e0bc079bcc881b973bda8099f
KevinLoayzaS <kevin28042001@gmail.com>
2023-01-11 10:25:14 +0100

    9/01

--- src/Session0901/Persona.java
+++ src/Session0901/Persona.java
@@ -6,22 +6,51 @@ public class Persona {
         private String nombre;
         private String apellidos;
 
+        private int edad;
+
 
         //metodos
     public Persona() //SI LO PONGO EN PRIVATE, EN LA CLASE "USO_PERSONA" NO IRIA BIEN
     {
-        apellidos="Loayza";
+
+
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
     }
 
     public String getDni() {
         return dni;
     }
 
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    //////////////////////////////////////////////
+    //////////////////////////////////////////////
+
     public String getApellidos() {
         return apellidos;
     }
 
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    /////////////////////////////////////////////
+    //////////////////////////////////////////////
+
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 }
--- src/Session0901/Uso_persona.java
+++ src/Session0901/Uso_persona.java
@@ -1,6 +1,34 @@
 package Session0901;
 
 public class Uso_persona {
-    Persona p1=new Persona();
+    public static void main(String[] args) {
+        Persona p1=new Persona();
+
+        System.out.println("El nombre de la primera persona es " +p1.getNombre());
+        System.out.println("El apellido de la primera persona es " +p1.getApellidos());
+        System.out.println("El dni de la primera persona es " +p1.getDni());
+        System.out.println("La edad de la primera persona es " +p1.getEdad());
+
+
+        p1.setDni("12345566");
+        p1.setNombre("Kevin");
+        p1.setApellidos("Loayza");
+
+
+        System.out.println("El nombre de la primera persona es " +p1.getNombre());
+        System.out.println("El apellido de la primera persona es " +p1.getApellidos());
+        System.out.println("El dni de la primera persona es " +p1.getDni());
+
+
+        Persona p2 = new Persona();
+
+        //LO MISMO QUE ARRIBA PERO CON P2.
+        //p2.setDni etc...
+        //p2.setNombre etc...
+        //p2.setApellidos etc...
+    }
+
+
+
 
 }
